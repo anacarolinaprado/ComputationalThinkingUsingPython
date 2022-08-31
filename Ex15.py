a6897e4ffddc434d64b65db5bcb8c81404a0031f
anacarolinaprado <111712865+anacarolinaprado@users.noreply.github.com>
2022-08-30 21:20:49 -0300

    atualizando exercício

--- Ex15.py
+++ Ex15.py
@@ -2,15 +2,14 @@ ladoA = int(input("Digite o valor A: "))
 ladoB = int(input("Digite o valor B: "))
 ladoC = int(input("Digite o valor C: "))
 
-if (ladoA + ladoB > ladoC):
-    if(ladoB + ladoC > ladoA):
-        if(ladoA + ladoC > ladoB):
-            print("Estes valores formam um triangulo")
-            if(ladoA != ladoB and ladoC != ladoB and ladoC != ladoA):
-                print("E este triângulo é ESCALENO")
-            elif(ladoA == ladoB and ladoA != ladoC or ladoC == ladoB and ladoC != ladoA or ladoC == ladoA and ladoC != ladoB):
-                print("E este triângulo é ISÓSCELES")
-            else:
-                print("E este triângulo é EQUILÁTERO")
+#Verifica se é ou não um triângulo
+if (((ladoA + ladoB) > ladoC) and ((ladoB + ladoC) > ladoA) and ((ladoA + ladoC) > ladoB)):
+    print("Estes valores formam um triangulo")
+    if(ladoA != ladoB and ladoC != ladoB and ladoC != ladoA):
+        print("E este triângulo é ESCALENO")
+    elif((ladoA == ladoB and ladoA != ladoC) or (ladoC == ladoB and ladoC != ladoA) or (ladoC == ladoA and ladoC != ladoB)):
+        print("E este triângulo é ISÓSCELES")
+    else:
+        print("E este triângulo é EQUILÁTERO")
 else:
     print("Esses valores não formam um triangulo!")
